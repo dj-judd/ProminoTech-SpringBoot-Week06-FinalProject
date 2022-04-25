1ad771243c131bb0291bdedbd0a2b673ccbd204d
dj-judd <dj.judd@gmail.com>
2022-04-24 19:41:16 -0500

    Setup Spring Tools Suite Project

---
+++ Promineo-Week6-FinalProject-DJ-PARM/src/main/java/com/promineo/PARM/PromineoWeek6FinalProjectDjParmApplication.java
@@ -0,0 +1,13 @@
+package com.promineo.PARM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromineoWeek6FinalProjectDjParmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromineoWeek6FinalProjectDjParmApplication.class, args);
+	}
+
+}
---
+++ Promineo-Week6-FinalProject-DJ-PARM/src/test/java/com/promineo/PARM/PromineoWeek6FinalProjectDjParmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promineo.PARM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromineoWeek6FinalProjectDjParmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
